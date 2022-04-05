9923012898fa3d973348318357437dcc0b24cb5d
jsowers13 <jsowers13@gmail.com>
2022-04-05 00:05:07 +0000

    Completed w/ Button

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+const suits = ["♦", "♥", "♠", "♣"];
+const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+var suit = suits[Math.floor(Math.random() * 3)];
+var value = values[Math.floor(Math.random() * 12)];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  if (suit == "♥" || suit == "♦") {
+    document.querySelector(".upperSuit").style.color = "red";
+    document.querySelector(".lowerSuit").style.color = "red";
+  }
+  document.querySelector(".upperSuit").innerHTML = suit;
+  document.querySelector(".lowerSuit").innerHTML = suit;
+  document.querySelector(".value").innerHTML = value;
+};
+
+window.clickFunction = function clickFunction() {
+  var newSuit = suits[Math.floor(Math.random() * 3)];
+  var newValue = values[Math.floor(Math.random() * 12)];
+  if (newSuit == "♥" || newSuit == "♦") {
+    document.querySelector(".upperSuit").style.color = "red";
+    document.querySelector(".lowerSuit").style.color = "red";
+  } else {
+    document.querySelector(".upperSuit").style.color = "black";
+    document.querySelector(".lowerSuit").style.color = "black";
+  }
+  document.querySelector(".upperSuit").innerHTML = newSuit;
+  document.querySelector(".lowerSuit").innerHTML = newSuit;
+  document.querySelector(".value").innerHTML = newValue;
 };
